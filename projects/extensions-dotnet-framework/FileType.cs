42fab4009a9d67daa0b5204296f8b22fbb90a8c6
Marcio de Freitas Nascimento <59565829+marcionascimentonb@users.noreply.github.com>
2021-04-08 19:43:17 -0300

    .

---
+++ projects/extensions-dotnet-framework/FileType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyProject.Models
+{
+    public enum FileType
+    {
+        [Display(Name = "Profile Image")]
+        Avatar = 1,
+        [Display(Name = "Pet Image")]
+        Pet
+    }
+}
---
+++ projects/extensions-dotnet-framework/MvcUtilities.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace MyProject.Models
+{
+    
+    /// <summary>
+    /// Helper methods for enums, lists
+    /// </summary>
+    public static class MvcUtilities
+    {
+
+        #region[UI Helpers]
+        /// <summary>
+        /// The word "this" here means the object that is calling the method
+        /// </summary>
+        /// <param name="val">value of the item from the Enum</param>
+        /// <returns></returns>
+        public static string ToDisplayString<T>(this T val)
+        {
+            DisplayAttribute[] attributes = (DisplayAttribute[])val
+               .GetType()
+               .GetField(val.ToString())
+               .GetCustomAttributes(typeof(DisplayAttribute), false);
+            return attributes.Length > 0 ? 
+                attributes[0].GetName() :
+                Enum.GetName(typeof(T), val);
+            
+        }
+        #endregion
+    }
+
+
+
+}
